fb5551cc022e94e00e7d2c9c76c69873337fbc6f
Eli Levy Karin <eli.levy.karin@gmail.com>
2018-05-31 18:15:17 +0200

    added some grouping logic

--- src/exonpredictor/grouppredictions.cpp
+++ src/exonpredictor/grouppredictions.cpp
@@ -13,87 +13,59 @@
 #include <limits>
 #include <cstdint>
 #include <queue>
+#include <string.h>
 #include "itoa.h"
 
 #ifdef OPENMP
 #include <omp.h>
 #endif
 
-const int PLUS = 1;
-const int MINUS = -1;
+const size_t EXPECTED_NUM_PREDICTIONS = 100000;
+
+struct prediction {
+    // constructor
+    prediction(unsigned int iProteinContigStrandId, unsigned int iProteinMMSeqs2Key, unsigned int iContigMMSeqs2Key, 
+                int iStrand, int iCombinedNormalizedAlnBitScore, unsigned int iNumExons, unsigned int iLowContigCoord, 
+                unsigned int iHighContigCoord, char * iExonCharptr) : proteinContigStrandId(iProteinContigStrandId), proteinMMSeqs2Key(iProteinMMSeqs2Key), 
+                contigMMSeqs2Key(iContigMMSeqs2Key), strand(iStrand),
+                combinedNormalizedAlnBitScore(iCombinedNormalizedAlnBitScore), numExons(iNumExons),
+                lowContigCoord(iLowContigCoord), highContigCoord(iHighContigCoord) {
+        // parsing exon info: 20317*20995*21701*21720*21753*21795\n
+        std::vector<std::string> exonIdsStr = Util::split(iExonCharptr, "\n");
+        std::vector<std::string> exonIdsSepStrs = Util::split(exonIdsStr[0].c_str(), "*");
+        for (size_t i = 0; i < exonIdsSepStrs.size(); ++i) {
+            exonIds.push_back(Util::fast_atoi<int>(exonIdsSepStrs[i].c_str()));
+        }
+
+        // initialize the cluster assignment as self (will change later):
+        clusterProteinContigStrandId = proteinContigStrandId;
+    }
+
+    unsigned int proteinContigStrandId;
+    unsigned int proteinMMSeqs2Key;
+    unsigned int contigMMSeqs2Key;
+    int strand;
+    int combinedNormalizedAlnBitScore;
+    unsigned int numExons;
+    unsigned int lowContigCoord;
+    unsigned int highContigCoord;
+    std::vector<unsigned int> exonIds;
+    unsigned int clusterProteinContigStrandId; // this will hold the cluster identifier
+};
 
 
 int grouppredictions(int argn, const char **argv, const Command& command) {
     LocalParameters& par = LocalParameters::getLocalInstance();
-    par.parseParameters(argn, argv, command, 4, true, true);
+    par.parseParameters(argn, argv, command, 2, true, true);
 
-    /*
-    // db1 = contigsDB (data + header):
-    std::string contigsDBFilename = par.db1;
-    std::string contigsDBIndexFilename = par.db1Index;
-    std::string contigsDBHeaderFilename(contigsDBFilename);
-    contigsDBHeaderFilename.append("_h");
-    std::string contigsDBHeaderIndexFilename(contigsDBFilename);
-    contigsDBHeaderIndexFilename.append("_h.index");
+    // Terminology: CS = Contig & Strand combination, TCS = Target & Contig & Strand (i.e. -  a prediction identifier)
+    // db1 = a map from CS to all its TCS predictions, ordered by their start on the contig, reverse subordered by # exons 
+    DBReader<unsigned int> contigStrandSortedMap(par.db1.c_str(), par.db1Index.c_str());
+    contigStrandSortedMap.open(DBReader<unsigned int>::LINEAR_ACCCESS);
 
-    DBReader<unsigned int> contigsData(contigsDBFilename.c_str(), contigsDBIndexFilename.c_str());
-    contigsData.open(DBReader<unsigned int>::NOSORT);
-
-    DBReader<unsigned int> contigsHeaders(contigsDBHeaderFilename.c_str(), contigsDBHeaderIndexFilename.c_str());
-    contigsHeaders.open(DBReader<unsigned int>::NOSORT);
-
-    // db2 = proteinsDB (the header is used while the data is needed for size computation)
-    std::string proteinsDBFilename = par.db2;
-    std::string proteinsDBHeaderFilename(proteinsDBFilename);
-    proteinsDBHeaderFilename.append("_h");
-    std::string proteinsDBHeaderIndexFilename(proteinsDBFilename);
-    proteinsDBHeaderIndexFilename.append("_h.index");
-
-    DBReader<unsigned int> proteinsHeaders(proteinsDBHeaderFilename.c_str(), proteinsDBHeaderIndexFilename.c_str());
-    proteinsHeaders.open(DBReader<unsigned int>::NOSORT);
-
-    std::string proteinsDBIndexFilename(par.db2);
-    proteinsDBIndexFilename.append(".index");
-    DBReader<unsigned int> proteinsData(par.db2.c_str(), proteinsDBIndexFilename.c_str());
-    proteinsData.open(DBReader<unsigned int>::NOSORT);
-    size_t numRecordsInDb = proteinsData.getSize();
-    size_t numCharactersInDb = proteinsData.getAminoAcidDBSize(); // method name is confusing...
-    size_t totNumOfAAsInProteinsDb = numCharactersInDb - (numRecordsInDb * 2); // \n and \0 for each record
-    proteinsData.close();
-    
-    // db3 = optimalExonsResuls (exons + map)
-    std::string combinedResultBasename = par.db3;
-    std::string optimalSetsExonRecordsFilename(combinedResultBasename);
-    optimalSetsExonRecordsFilename.append("_optimal_exon_sets");
-    std::string optimalSetsExonRecordsIndexFilename(optimalSetsExonRecordsFilename);
-    optimalSetsExonRecordsIndexFilename.append(".index");
-
-    std::string setsMapFilename(combinedResultBasename);
-    setsMapFilename.append("_protein_contig_strand_map");
-    std::string setsMapIndexFilename(setsMapFilename);
-    setsMapIndexFilename.append(".index");
-
-    DBReader<unsigned int> optimalSetsExonRecords(optimalSetsExonRecordsFilename.c_str(), optimalSetsExonRecordsIndexFilename.c_str());
-    optimalSetsExonRecords.open(DBReader<unsigned int>::LINEAR_ACCCESS);
-
-    DBReader<unsigned int> setMap(setsMapFilename.c_str(), setsMapIndexFilename.c_str());
-    setMap.open(DBReader<unsigned int>::LINEAR_ACCCESS);
-    
-    // db4 = output
-    std::string outDBFilename(par.db4);
-    std::string outDBIndexFilename(par.db4);
-    outDBIndexFilename.append(".index");
-
-    std::string outDBHeaderFilename(par.db4);
-    outDBHeaderFilename.append("_h");
-    std::string outDBHeaderIndexFilename(par.db4);
-    outDBHeaderIndexFilename.append("_h.index");
-
-    DBWriter concatenatedSetsHeaders(outDBHeaderFilename.c_str(), outDBHeaderIndexFilename.c_str(), par.threads);
-    concatenatedSetsHeaders.open();
-
-    DBWriter concatenatedSetsData(outDBFilename.c_str(), outDBIndexFilename.c_str(), par.threads);
-    concatenatedSetsData.open();
+    // db2 = output, cluster format
+    DBWriter writerGroupedPredictions(par.db2.c_str(), par.db2Index.c_str(), par.threads);
+    writerGroupedPredictions.open();
 
 #pragma omp parallel
     {
@@ -102,146 +74,97 @@ int grouppredictions(int argn, const char **argv, const Command& command) {
         thread_idx = static_cast<unsigned int>(omp_get_thread_num());
 #endif
         // per thread variables
-        std::ostringstream joinedHeaderStream;
-        std::ostringstream joinedExonsStream;
-        std::string littleStringBuffer;
-        littleStringBuffer.reserve(1000);
+        std::vector<prediction> predictionToCluster;
+        predictionToCluster.reserve(EXPECTED_NUM_PREDICTIONS);
 
-        char *entry[255];      
+        char *entry[255];       
         
 #pragma omp for schedule(dynamic, 100)
-        for (size_t id = 0; id < optimalSetsExonRecords.getSize(); id++) {
+        for (size_t id = 0; id < contigStrandSortedMap.getSize(); id++) {
             Debug::printProgress(id);
 
-            unsigned int setCombinationKey = optimalSetsExonRecords.getDbKey(id);
+            // these will serve to verify sorted order:
+            unsigned int prevLowCoord = 0;
+            unsigned int prevNumExons = 0;
 
-            // take the record corresponding to the same id from both result files:
-            char *optimalExonRecord = optimalSetsExonRecords.getData(id);
-            char *setRecord = setMap.getData(id);
-            
-            // parse setRecord (a single line)
-            const size_t setColumns = Util::getWordsOfLine(setRecord, entry, 255);
-            if (setColumns != 9) {
-                Debug(Debug::ERROR) << "ERROR: the map record should contain 9 columns: contigStrandId, proteinMMSeqs2Key, contigMMSeqs2Key, strand, combinedBitScore, numExons, lowContigCoord, highContigCoord, exonIDsStr. This doesn't seem to be the case.\n";
-                EXIT(EXIT_FAILURE);
-            }
-            //unsigned int contigAndStrandId = Util::fast_atoi<int>(entry[0]);
-            unsigned int proteinMMSeqs2Key = Util::fast_atoi<int>(entry[1]);
-            unsigned int contigMMSeqs2Key = Util::fast_atoi<int>(entry[2]);
-            int strand = Util::fast_atoi<int>(entry[3]);
-            int combinedNormalizedAlnBitScore = Util::fast_atoi<int>(entry[4]);
-            unsigned int numExons = Util::fast_atoi<int>(entry[5]);
-            unsigned int lowContigCoord = Util::fast_atoi<int>(entry[6]);
-            unsigned int highContigCoord = Util::fast_atoi<int>(entry[7]);
-            setRecord = Util::skipLine(setRecord);
+            char *contigStrandSortedRecord = contigStrandSortedMap.getData(id);
+            while (*contigStrandSortedRecord != '\0') {
+                const size_t contigStrandSortedColumns = Util::getWordsOfLine(contigStrandSortedRecord, entry, 255);
 
-            // get non-MMSeqs2 identifiers from header files:
-            const char* contigHeader = contigsHeaders.getDataByDBKey(contigMMSeqs2Key);
-            const char* proteinHeader = proteinsHeaders.getDataByDBKey(proteinMMSeqs2Key);
-            
-            // get contig data:
-            const char* contigData = contigsData.getDataByDBKey(contigMMSeqs2Key);
-            //size_t totNumNucleotidesInContig = strlen(contigData);
-            
-            // approximate the total Evalue:
-            // Evalue = m X n * 2^(-S), where m = totNumOfAAsInProteinsDb, n = twoStrands, S = combinedNormalizedAlnBitScore
-            double log2Evalue = log2(totNumOfAAsInProteinsDb) + log2(2) - combinedNormalizedAlnBitScore;
-            double combinedEvalue = pow(2, log2Evalue);
-            
-            // initialize header:
-            size_t numWhiteCharsToTrim = 2; // a single space and a \n
-            littleStringBuffer.append(proteinHeader, std::strlen(proteinHeader) - numWhiteCharsToTrim);
-            joinedHeaderStream << littleStringBuffer << "|";
-            littleStringBuffer.clear();
-            littleStringBuffer.append(contigHeader, std::strlen(contigHeader) - numWhiteCharsToTrim);
-            joinedHeaderStream << littleStringBuffer << "|";
-            littleStringBuffer.clear();
-            (strand == PLUS) ? joinedHeaderStream << "+" : joinedHeaderStream << "-";
-            joinedHeaderStream << "|" << combinedNormalizedAlnBitScore << "|" << combinedEvalue << "|" << numExons << "|" << lowContigCoord << "|" << highContigCoord;
-            
-            int lastTargetPosMatched = 0;
-            while (*optimalExonRecord != '\0') {
-                const size_t exonRecordColumns = Util::getWordsOfLine(optimalExonRecord, entry, 255);
-
-                if (exonRecordColumns != 10) {
-                    Debug(Debug::ERROR) << "ERROR: the exon record should contain 10 columnn (alignment format). This doesn't seem to be the case.\n";
+                if (contigStrandSortedColumns != 9) {
+                    Debug(Debug::ERROR) << "ERROR: the sorted contig+strand map record should contain 9 columns: proteinContigStrandId, proteinMMSeqs2Key, contigMMSeqs2Key, strand, combinedBitScore, numExons, lowContigCoord, highContigCoord, exonIDsStr. This doesn't seem to be the case.\n";
                     EXIT(EXIT_FAILURE);
                 }
 
-                // unsigned int exonMMSeqs2Key = Util::fast_atoi<int>(entry[0]);
-                // int exonToProteinAlnScore = Util::fast_atoi<int>(entry[1]);
-                // double exonSequenceIdentity = atof(entry[2]);
-                // double exonEvalue = atof(entry[3]);
-                int proteinMatchStart =  Util::fast_atoi<int>(entry[4]);
-                int proteinMatchEnd = Util::fast_atoi<int>(entry[5]);
-                // int proteinLen = Util::fast_atoi<int>(entry[6]);
-                int exonContigStart = Util::fast_atoi<int>(entry[7]);
-                int exonContigEnd = Util::fast_atoi<int>(entry[8]);
-                int exonNucleotideLen = Util::fast_atoi<int>(entry[9]);
+                unsigned int proteinContigStrandId = Util::fast_atoi<int>(entry[0]);
+                unsigned int proteinMMSeqs2Key = Util::fast_atoi<int>(entry[1]);
+                unsigned int contigMMSeqs2Key = Util::fast_atoi<int>(entry[2]);
+                int strand = Util::fast_atoi<int>(entry[3]);
+                int combinedNormalizedAlnBitScore = Util::fast_atoi<int>(entry[4]);
+                unsigned int numExons = Util::fast_atoi<int>(entry[5]);
+                unsigned int lowContigCoord = Util::fast_atoi<int>(entry[6]);
+                unsigned int highContigCoord = Util::fast_atoi<int>(entry[7]);
+                char * exonCharptr = entry[8];
 
-                if (strand == MINUS) {
-                    if ((exonContigStart > 0) || (exonContigEnd > 0)) {
-                        Debug(Debug::ERROR) << "ERROR: strand is MINUS but the contig coordinates are positive. Soemthing is wrong.\n";
-                        EXIT(EXIT_FAILURE);
-                    }
+                // verify sorted order:
+                if (prevNumExons == 0) {
+                    // first iteration:
+                    prevNumExons = numExons;
+                    prevLowCoord = lowContigCoord;
+                }
+                else if (prevLowCoord > lowContigCoord) {
+                    Debug(Debug::ERROR) << "ERROR: Predictions are assumed to be sorted by their start position. This doesn't seem to be the case.\n";
+                    EXIT(EXIT_FAILURE);
+                }
+                else if ((prevLowCoord == lowContigCoord) && (prevNumExons < numExons)) {
+                    Debug(Debug::ERROR) << "ERROR: Predictions are assumed to be reverse sub-sorted by their number of exons. This doesn't seem to be the case.\n";
+                    EXIT(EXIT_FAILURE);
+                }
+                else {
+                    prevNumExons = numExons;
+                    prevLowCoord = lowContigCoord;
                 }
 
-                // in order to avoid target overlaps, we remove a few codons from the start of the current exon if needed:
-                int exonAdjustedContigStart = exonContigStart;
-                int exonAdjustedNucleotideLen = exonNucleotideLen;
-                if (lastTargetPosMatched > proteinMatchStart) {
-                    int diffInAAs = lastTargetPosMatched - proteinMatchStart + 1;
-                    exonAdjustedContigStart += (3 * diffInAAs * strand);
-                    exonAdjustedNucleotideLen -= (3 * diffInAAs);
-                }
-                int lowContigCoord = (strand == PLUS) ? exonAdjustedContigStart : (-1 * exonContigEnd);
-                // extract the segment from the contig:
-                std::string exonContigSeq(&contigData[lowContigCoord], (size_t)exonAdjustedNucleotideLen);
-                // update the last AA of the protein that was matched:
-                lastTargetPosMatched = proteinMatchEnd;
+                predictionToCluster.emplace_back(proteinContigStrandId, proteinMMSeqs2Key, contigMMSeqs2Key, strand, combinedNormalizedAlnBitScore, numExons, lowContigCoord, highContigCoord, exonCharptr);
 
-                // write the header and data to streams:
-                joinedHeaderStream << "|" << abs(exonContigStart) << "[" << abs(exonAdjustedContigStart) << "]:";
-                joinedHeaderStream << abs(exonContigEnd) << "[" << abs(exonContigEnd) << "]:";
-                joinedHeaderStream << exonNucleotideLen << "[" << exonAdjustedNucleotideLen << "]";
-
-                if (strand == PLUS) {
-                    joinedExonsStream << exonContigSeq;
-                } else {
-                    std::string exonContigRevCompSeq(exonContigSeq);
-                    reverseComplement (exonContigSeq, exonContigRevCompSeq);
-                    joinedExonsStream << exonContigRevCompSeq;
-                }
-
-                optimalExonRecord = Util::skipLine(optimalExonRecord);
+                contigStrandSortedRecord = Util::skipLine(contigStrandSortedRecord);
             }
 
-            // finished collecting all exons for the current combination, write streams:
-            joinedHeaderStream << "\n";
-            std::string headerToWrite = joinedHeaderStream.str();
-            concatenatedSetsHeaders.writeData(headerToWrite.c_str(), headerToWrite.length(), setCombinationKey, thread_idx);
-            joinedHeaderStream.str("");
+            // by this stage we have collected all TCS predictions into a vector
+            for (size_t i = 0; i < predictionToCluster.size(); ++i) {
+                for (size_t j = (i + 1); j < predictionToCluster.size(); ++j) {
+                    if (predictionToCluster[j].lowContigCoord >= predictionToCluster[i].highContigCoord) {
+                        // overlap is over - no need to compare to any more j - moving to the next i
+                        break;
+                    }
 
-            joinedExonsStream << "\n";
-            std::string dataToWrite = joinedExonsStream.str();
-            concatenatedSetsData.writeData(dataToWrite.c_str(), dataToWrite.length(), setCombinationKey, thread_idx);
-            joinedExonsStream.str("");
+                    bool doIandJshareAnExon = false;
+                    for (size_t exon_id_i = 0; exon_id_i < predictionToCluster[i].exonIds.size(); ++exon_id_i) {
+                        for (size_t exon_id_j = 0; exon_id_j < predictionToCluster[j].exonIds.size(); ++exon_id_j) {
+                            if (predictionToCluster[i].exonIds[exon_id_i] == predictionToCluster[j].exonIds[exon_id_j]) {
+                                doIandJshareAnExon = true;
+                                goto endExonComparison;
+                            }
+                        }
+                    }
 
+                    // assign j to the cluster of i if it has a mutual exon and if it wasn't already assigned:
+                    endExonComparison:
+                    if (doIandJshareAnExon && (predictionToCluster[j].proteinContigStrandId == predictionToCluster[j].clusterProteinContigStrandId)) {
+                        predictionToCluster[j].clusterProteinContigStrandId = predictionToCluster[i].proteinContigStrandId;
+                    }
+                }
+            }
         }
     }
 
+    
     // cleanup
-    concatenatedSetsHeaders.close();
-    concatenatedSetsData.close();
-    contigsData.close();
-    contigsHeaders.close();
-    proteinsHeaders.close();
-    optimalSetsExonRecords.close();
-    setMap.close();
+    writerGroupedPredictions.close();
+    contigStrandSortedMap.close();
     
     Debug(Debug::INFO) << "\nDone.\n";
 
-    */
 
     return EXIT_SUCCESS;
 }
