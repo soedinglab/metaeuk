40c58df9393f90fe4bd19aa8b2015e502db68858
Eli Levy Karin <eli.levy.karin@gmail.com>
2018-08-06 17:35:57 +0200

    dont compile inside the test

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -e
 
-git clone git@github.com:soedinglab/metaeuk.git
-cd metaeuk
-git submodule init
-git submodule update
-mkdir build
-cd build/
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-make -j 8
-cd ../..
-
-./test.sh metaeuk/build/src/metaeuk minus_strand minus_strand_results
-./test.sh metaeuk/build/src/metaeuk multi_exon multi_exon_results
-./test.sh metaeuk/build/src/metaeuk two_contigs two_contigs_results
+./test.sh "$1" minus_strand minus_strand_results
+./test.sh "$1" multi_exon multi_exon_results
+./test.sh "$1" two_contigs two_contigs_results
