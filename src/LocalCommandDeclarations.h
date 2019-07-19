786e2c73098a3521e773660005ceb9b59341f00d
Eli Levy Karin <eli.levy.karin@gmail.com>
2019-07-19 11:39:20 +0200

    updated mmseqs2 for slice, commands cleanup

--- src/LocalCommandDeclarations.h
+++ src/LocalCommandDeclarations.h
@@ -10,5 +10,6 @@ extern int reduceredundancy(int argc, const char **argv, const Command& command)
 extern int grouppredictions(int argn, const char **argv, const Command& command);
 extern int assigntaxonomy(int argc, const char **argv, const Command& command);
 extern int unitetoseqdbs(int argc, const char **argv, const Command& command);
+extern int swapdb(int argc, const char **argv, const Command& command);
 
 #endif
