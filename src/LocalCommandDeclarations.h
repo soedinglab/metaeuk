0f504599562fed9e4093c51a37e611ee1f800df5
Eli Levy Karin <eli.levy.karin@gmail.com>
2019-08-21 15:39:35 +0200

    add groupstoacc module to map representatives to members

--- src/LocalCommandDeclarations.h
+++ src/LocalCommandDeclarations.h
@@ -3,12 +3,13 @@
 
 #include "Command.h"
 
+extern int resultspercontig(int argc, const char **argv, const Command& command);
 extern int predictexons(int argc, const char **argv, const Command& command);
 extern int collectoptimalset(int argn, const char **argv, const Command& command);
 extern int unitesetstofasta(int argn, const char **argv, const Command& command);
 extern int reduceredundancy(int argc, const char **argv, const Command& command);
+extern int groupstoacc(int argc, const char **argv, const Command& command);
 extern int assigntaxonomy(int argc, const char **argv, const Command& command);
-extern int resultspercontig(int argc, const char **argv, const Command& command);
 extern int swapdb(int argc, const char **argv, const Command& command);
 
 #endif
---
+++ src/exonpredictor/groupstoacc.cpp
@@ -0,0 +1,105 @@
+#include "LocalParameters.h"
+#include "PredictionParser.h"
+#include "DBReader.h"
+#include "DBWriter.h"
+#include "Debug.h"
+#include "Util.h"
+#include "FileUtil.h"
+#include "MathUtil.h"
+#include "itoa.h"
+
+#ifdef OPENMP
+#include <omp.h>
+#endif
+
+int groupstoacc(int argn, const char **argv, const Command& command) {
+    LocalParameters& par = LocalParameters::getLocalInstance();
+    par.parseParameters(argn, argv, command, true, 0, 0);
+
+    // db1 = input, contigsDB (only the header is used)
+    DBReader<unsigned int> contigsHeaders(par.hdr1.c_str(), par.hdr1Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
+    contigsHeaders.open(DBReader<unsigned int>::NOSORT);
+
+    // db2 = input, targetsDB (only the header is used)
+    DBReader<unsigned int> targetsHeaders(par.hdr2.c_str(), par.hdr2Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
+    targetsHeaders.open(DBReader<unsigned int>::NOSORT);
+
+    // db3 = input, grouping of predictions: T,S of representatives to T,S of prediction
+    DBReader<unsigned int> readerRepToMembers(par.db3.c_str(), par.db3Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
+    readerRepToMembers.open(DBReader<unsigned int>::LINEAR_ACCCESS);
+
+    DBWriter writer(par.db4.c_str(), par.db4Index.c_str(), par.threads, par.compressed, Parameters::DBTYPE_OMIT_FILE);
+    writer.open();
+
+    Debug::Progress progress(readerRepToMembers.getSize());
+#pragma omp parallel
+    {
+        unsigned int thread_idx = 0;
+#ifdef OPENMP
+        thread_idx = static_cast<unsigned int>(omp_get_thread_num());
+#endif
+        // per thread variables
+        std::string strToWrite;
+        const char *entry[255];
+        
+#pragma omp for schedule(dynamic, 100)
+        for (size_t id = 0; id < readerRepToMembers.getSize(); id++) {
+            progress.updateProgress();
+
+            unsigned int contigKey = readerRepToMembers.getDbKey(id);
+
+            char *results = readerRepToMembers.getData(id, thread_idx);
+
+            // get contig header:
+            const char* contigHeader = contigsHeaders.getDataByDBKey(contigKey, thread_idx);
+            std::string contigHeaderAcc = Util::parseFastaHeader(contigHeader);
+
+            // process a specific contig
+            while (*results != '\0') {
+                const size_t columns = Util::getWordsOfLine(results, entry, 255);
+                // each line informs of a representative and of a member
+                if (columns != 4) {
+                    Debug(Debug::ERROR) << "There should be 4 columns in the input file. This doesn't seem to be the case.\n";
+                    EXIT(EXIT_FAILURE);
+                }
+
+                unsigned int repTargetKey = Util::fast_atoi<int>(entry[0]);
+                int repStrand = Util::fast_atoi<int>(entry[1]);
+                unsigned int memTargetKey = Util::fast_atoi<int>(entry[2]);
+                int memStrand = Util::fast_atoi<int>(entry[3]);
+
+                if (repStrand != memStrand) {
+                    Debug(Debug::ERROR) << "A representative should always be on the same strand as its member. This doesn't seem to be the case.\n";
+                    EXIT(EXIT_FAILURE);
+                }
+
+                std::string strandStr = "+";
+                if (repStrand == MINUS) {
+                    strandStr = "-";
+                }
+
+                const char* repTargetHeader = targetsHeaders.getDataByDBKey(repTargetKey, thread_idx);
+                std::string repTargetHeaderAcc = Util::parseFastaHeader(repTargetHeader);
+
+                const char* memTargetHeader = targetsHeaders.getDataByDBKey(memTargetKey, thread_idx);
+                std::string memTargetHeaderAcc = Util::parseFastaHeader(memTargetHeader);
+                
+                strToWrite.clear();
+                strToWrite = repTargetHeaderAcc + "|" + contigHeaderAcc + "|" + strandStr + "\t" +
+                             memTargetHeaderAcc + "|" + contigHeaderAcc + "|" + strandStr + "\n";
+
+                writer.writeData(strToWrite.c_str(), strToWrite.size(), 0, thread_idx, false, false);
+
+                results = Util::skipLine(results);
+            }
+        }
+    }
+    writer.close(true);
+    FileUtil::remove(par.db4Index.c_str());
+
+    contigsHeaders.close();
+    targetsHeaders.close();
+    readerRepToMembers.close();
+
+    return EXIT_SUCCESS;
+}
