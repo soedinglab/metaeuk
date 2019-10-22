90c926d4871d6af60ccfa5fe83dc13f56d28ffd6
Eli Levy Karin <eli.levy.karin@gmail.com>
2019-10-22 15:15:27 +0200

    added test for target cov parameter

--- tests/run.sh
+++ tests/run.sh
@@ -9,6 +9,7 @@
 ./test.sh "$1" two_contigs two_contigs_results -1 0
 ./test.sh "$1" target_overlap target_overlap_results -3 0
 ./test.sh "$1" cluster_rep cluster_rep_results -3 0
+./test.sh "$1" target_cov target_cov_results -1 0.7
 
 ./test_no_overlap.sh "$1" no_overlap no_overlap_results
 
