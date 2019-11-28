684c33ef1fdb8cdd8b937f91660fdec0a644aa0e
Eli Levy Karin <eli.levy.karin@gmail.com>
2019-11-28 15:47:23 +0100

    changed test to use the easy-predict workflow

--- tests/test_no_overlap.sh
+++ tests/test_no_overlap.sh
@@ -5,15 +5,9 @@ DATAPATH="$2"
 RESULTPATH="$3"
 
 mkdir -p "${RESULTPATH}"
-"${METAEUK}" createdb "${DATAPATH}/contigs.fna" "${RESULTPATH}/contigs" --dbtype 2
-"${METAEUK}" createdb "${DATAPATH}/proteins.faa" "${RESULTPATH}/proteins" --dbtype 1
 
-"${METAEUK}" predictexons "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predEx" tempFolder --metaeuk-eval 0.0001 -e 100 --min-length 20 --metaeuk-tcov 0
-"${METAEUK}" reduceredundancy "${RESULTPATH}/predEx" "${RESULTPATH}/predRedOverAllowed" "${RESULTPATH}/predClust" --overlap 1
-"${METAEUK}" unitesetstofasta "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predRedOverAllowed" "${RESULTPATH}/predRedOverAllowed.fas" --protein 1
-"${METAEUK}" reduceredundancy "${RESULTPATH}/predEx" "${RESULTPATH}/predRedNoOver" "${RESULTPATH}/predClust" --overlap 0
-"${METAEUK}" unitesetstofasta "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predRedNoOver" "${RESULTPATH}/predRedNoOver.fas" --protein 1
-"${METAEUK}" groupstoacc "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predClust" "${RESULTPATH}/predClust.tsv"
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/predRedNoOver.fas" tempFolder --overlap 0 --metaeuk-tcov 0 --protein 1
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/predRedOverAllowed.fas" tempFolder --overlap 1 --metaeuk-tcov 0 --protein 1
 
 # check output #
 perl compare_fasta_results.pl "${RESULTPATH}/predRedOverAllowed.fas" "${RESULTPATH}/predRedNoOver.fas" "${DATAPATH}/as_should_final_grouped_predictions_rep.fas" "${DATAPATH}/as_should_final_grouped_predictions_rep_no_overlap.fas"
