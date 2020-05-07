c7e84c9eb88e25eb0d1538889092a17755093a78
Eli Levy Karin <eli.levy.karin@gmail.com>
2020-05-07 14:20:01 +0200

    update tests after both codon and aa change

--- tests/test.sh
+++ tests/test.sh
@@ -12,10 +12,10 @@ mkdir -p "${RESULTPATH}"
 "${METAEUK}" createdb "${DATAPATH}/proteins.faa" "${RESULTPATH}/proteins" --dbtype 1
 
 "${METAEUK}" predictexons "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predEx" "${RESULTPATH}/tempFolder" --set-gap-extend "${SETGAPEXTEND}" --metaeuk-tcov "${MINTARGETCOV}"
-"${METAEUK}" unitesetstofasta "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predEx" "${RESULTPATH}/predEx.fas" --protein 1
+"${METAEUK}" unitesetstofasta "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predEx" "${RESULTPATH}/predExFlat"
 "${METAEUK}" reduceredundancy "${RESULTPATH}/predEx" "${RESULTPATH}/predRed" "${RESULTPATH}/predClust"
-"${METAEUK}" unitesetstofasta "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predRed" "${RESULTPATH}/predRed.fas" --protein 1
+"${METAEUK}" unitesetstofasta "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predRed" "${RESULTPATH}/predRedFlat"
 "${METAEUK}" groupstoacc "${RESULTPATH}/contigs" "${RESULTPATH}/proteins" "${RESULTPATH}/predClust" "${RESULTPATH}/predClust.tsv"
 
 # check ungrouped and grouped output
-perl compare_fasta_results.pl "${RESULTPATH}/predEx.fas" "${RESULTPATH}/predRed.fas" "${DATAPATH}/as_should_final_united_exons_aa.fas" "${DATAPATH}/as_should_final_grouped_predictions_rep.fas"
+perl compare_fasta_results.pl "${RESULTPATH}/predExFlat.fas" "${RESULTPATH}/predRedFlat.fas" "${DATAPATH}/as_should_final_united_exons_aa.fas" "${DATAPATH}/as_should_final_grouped_predictions_rep.fas"
--- tests/test_no_overlap.sh
+++ tests/test_no_overlap.sh
@@ -6,8 +6,8 @@ RESULTPATH="$3"
 
 mkdir -p "${RESULTPATH}"
 
-"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/predRedNoOver.fas" tempFolder --overlap 0 --metaeuk-tcov 0 --protein 1
-"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/predRedOverAllowed.fas" tempFolder --overlap 1 --metaeuk-tcov 0 --protein 1
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/predRedNoOver" tempFolder --overlap 0 --metaeuk-tcov 0
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/predRedOverAllowed" tempFolder --overlap 1 --metaeuk-tcov 0
 
 # check output #
 perl compare_fasta_results.pl "${RESULTPATH}/predRedOverAllowed.fas" "${RESULTPATH}/predRedNoOver.fas" "${DATAPATH}/as_should_final_grouped_predictions_rep.fas" "${DATAPATH}/as_should_final_grouped_predictions_rep_no_overlap.fas"
